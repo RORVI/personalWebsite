9ee2113f5c9ba4f34864bffd5122e66b60898fd4
Ovidiu Roman <ovidiurobert.roman@gmail.com>
2024-06-26 20:45:19 +0300

    Small improvements on the js code

--- js/allscript.js
+++ js/allscript.js
@@ -15,7 +15,6 @@ $(window).on('load', function(){
             subMessage: "",
         };
     }
-    
 });
 
 $(document).on("click", ".tab-controll" , function(e) {
@@ -31,4 +30,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var coll = document.getElementsByClassName("collapsible");
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+});
+
 
